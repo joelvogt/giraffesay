44dd94d748a06e39bf1df52ef87ad1554ebfc10a
joelvogt <joel.vogt@evrythng.com>
2017-03-29 12:28:59 +0100

    cleanup

--- leakingkey.sh
+++
@@ -1,1 +0,0 @@
- curl -X GET 'https://api-staging.evrythng.net/access' -H "Authorization:tLXAp8poJ3hKmsKEfsxHwqCSdcQgyYpMBdTDeoQqeR41MGjTAONvSBP2ZEyDvW8apUJVVXhuK0xiioIw" -H "Content-Type: application/json"

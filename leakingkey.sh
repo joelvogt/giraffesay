bd2c344d17db7fe0d598f9c2566bdc3c410acae4
joelvogt <joel.vogt@evrythng.com>
2017-03-29 12:22:51 +0100

    pre-commit hook to rpevent key leaks

---
+++ leakingkey.sh
@@ -0,0 +1,1 @@
+ curl -X GET 'https://api-staging.evrythng.net/access' -H "Authorization:tLXAp8poJ3hKmsKEfsxHwqCSdcQgyYpMBdTDeoQqeR41MGjTAONvSBP2ZEyDvW8apUJVVXhuK0xiioIw" -H "Content-Type: application/json"
